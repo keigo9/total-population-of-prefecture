cf8f2053f6525f4a9045a2ad9f3b90ea94bb6ad6
keigo9 <keigo.matsunari@gmail.com>
2024-01-28 13:49:54 +0900

    feature/make-header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <nav class="header">
+    <h1>{{ msg }}</h1>
+  </nav>
+</template>
+
+<style scoped>
+.header {
+  margin-bottom: 2.5rem;
+}
+.header h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  text-align: center;
+  background-color: green;
+}
+
+@media (max-width: 1024px) {
+  .headerh1 {
+    font-size: 1.5rem;
+  }
+}
+</style>
